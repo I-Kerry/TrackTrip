0cedc35c1b31573c1212e06ecef7a7f5c28641c5
Kirill Maidanovich <kerrykool@inbox.ru>
2026-06-09 21:16:00 +0800

    Initial Commit

---
+++ TrackTrip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrackTrip
+//
+//  Created by Kirill Maidanovich on 2026/6/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrackTrip/TrackTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackTripApp.swift
+//  TrackTrip
+//
+//  Created by Kirill Maidanovich on 2026/6/9.
+//
+
+import SwiftUI
+
+@main
+struct TrackTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
